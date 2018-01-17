f13a509728b4fe63026f2982418a97c4c222c190
ezambomsantana <ezambomsantana@gmail.com>
2018-01-17 16:14:05 -0200

    count containers R script

---
+++ count_containers.R
@@ -0,0 +1,26 @@
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args)!=2) {
+  stop("At least two arguments must be supplied (working directory and input file)", call.=FALSE)
+} else {
+  setwd(args[1])
+  data <- read.csv(args[2], header=TRUE, sep=',', colClasses= c("integer","integer","integer","integer"),)
+}
+
+data$minute <- seq.int(nrow(data))
+
+require("ggplot2")
+
+theme_set(theme_gray(base_size = 18))
+png('containers.png')
+ggplot(data=data, aes(x=minute)) +
+  geom_line(aes(y = data.collector, colour = "Data Collector")) + 
+  geom_line(aes(y = resource.catalog, colour = "Resource Catalog")) + 
+  geom_line(aes(y = resource.discovery, colour = "Resource Discovery")) + 
+  geom_line(aes(y = kong, colour = "Kong")) + 
+  scale_colour_manual("", 
+                      breaks = c("Data Collector", "Resource Catalog", "Resource Discovery", "Kong"),
+                      values = c("red", "green", "blue", "black")) +
+  xlab("Experiment Time (m)") + ylab("Containers")
+dev.off()
+
